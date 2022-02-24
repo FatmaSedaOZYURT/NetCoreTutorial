9d8c121d02267c2891acf88414213c221e2bbd84
Fatma Seda <fsozyurt@hotmail.com>
2022-02-24 17:00:09 +0300

    HotelFinder

---
+++ HotelFinder.DataAccess/HotelDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelFinder.DataAccess
+{
+    public class HotelDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer();
+        }
+    }
+}
