40c9e675a0a96d5f99d961148890d345ce207bbe
Fatma Seda <fsozyurt@hotmail.com>
2022-02-25 09:30:02 +0300

    Singleton

---
+++ HotelFinder.API/Controllers/HotelsController.cs
@@ -0,0 +1,49 @@
+﻿using HotelFinder.Business.Abstract;
+using HotelFinder.Business.Concrete;
+using HotelFinder.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelFinder.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelsController : ControllerBase
+    {
+        IHotelService _hotelServices;
+        public HotelsController(IHotelService hotelServices)
+        {
+            _hotelServices = hotelServices;
+        }
+
+        [HttpGet]
+        public List<Hotel> Get()
+        {
+            return _hotelServices.GetAllHotels();
+        }
+        [HttpGet("{id}")]
+        public Hotel Get(int id)
+        {
+            return _hotelServices.GetHotelByID(id);
+        }
+        [HttpPost]
+        public Hotel Post([FromBody]Hotel hotel)
+        {
+            return _hotelServices.CreateHotel(hotel);
+        }
+        [HttpPut]
+        public Hotel Put([FromBody]Hotel hotel)
+        {
+            return _hotelServices.UpdateHotel(hotel);
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _hotelServices.DeleteHotel(id);
+        }
+    }
+}
--- HotelFinder.DataAccess/HotelDbContext.cs
+++ HotelFinder.DataAccess/HotelDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using HotelFinder.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +13,9 @@ namespace HotelFinder.DataAccess
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer();
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Database=HotelDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
+
+        public DbSet<Hotel> Hotels { get; set; }
     }
 }
